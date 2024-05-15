c1ab135c65b64c0020e7dc2f98ab865b875e4b87
Yoga Fradana <108604546+Yogafradana@users.noreply.github.com>
2024-05-15 13:59:09 +0700

    -

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h1>Bug Membandel? Temukan</h1>
+        <h2>Solusinya Sambil Ngopi di</h2>
+        <h3>Cafe Microdata</h3>
+        <button>Pesan Sekarang</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
