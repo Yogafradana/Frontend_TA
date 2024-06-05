141df000e5ecfb85a7dca35a6becbd03211ccd40
dianafnioktavia23 <113124849+dianafnioktavia23@users.noreply.github.com>
2024-06-05 23:22:38 +0700

    update

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,51 +1,67 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
 import "./ExploreMenu.css";
+import axios from "axios";
 
-const ExploreMenu = ({ category, setCategory }) => {
-  const [menuList, setMenuList] = useState([]);
+const ExploreMenu = ({ category }) => {
+  const [bestSellerResponse, setBestSellerResponse] = useState(null);
+  const [recommendationResponse, setRecommendationResponse] = useState(null);
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/kategori') // Ganti dengan URL API yang benar
-      .then(response => {
-        setMenuList(response.data);
-      })
-      .catch(error => {
-        console.error("There was an error fetching the menu data!", error);
-      });
+    getBestSellerMenu();
+    getRecommendationMenu();
   }, []);
 
-  return (
-    <div className="explore-menu" id="explore-menu">
-      <h1>Explore Our Menu</h1>
-      <p className="explore-menu-text">
-        In this single tutorial you will learn to build a food ordering website
-        / app step by step using React JS. In this React JS project we will
-        create the Home page to display the
-      </p>
-      <div className="explore-menu-list">
-        {menuList.map((item, index) => {
-          return (
-            <div
-              onClick={() =>
-                setCategory((prev) =>
-                  prev === item.nama_kategori ? "All" : item.nama_kategori
-                )
-              }
-              key={index}
-              className="explore-menu-list-item"
-            >
-              <img
-                className={category === item.nama_kategori ? "active" : ""}
-                src={item.image}
-                alt={item.nama_kategori}
-              />
-              <p>{item.nama_kategori}</p>
+  const getBestSellerMenu = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/api/menu/best");
+      setBestSellerResponse(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getRecommendationMenu = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/api/menu/rekomendasi");
+      setRecommendationResponse(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const renderMenuItems = (items) => {
+    return items.map((item, index) => {
+      if (category === "All" || category === item.category) {
+        const imageUrl = `http://127.0.0.1:8081/images/${item.gambar}`;
+        return (
+          <div className="card" key={index}>
+            <img src={imageUrl} className="card-img-top" alt={item.nama_menu} />
+            <div className="card-body">
+              <h5 className="card-title">{item.nama_menu}</h5>
+              <p className="card-text">{item.deskripsi}</p>
+              <p className="card-text text-right font-weight-bold">{item.harga}</p>
             </div>
-          );
-        })}
+          </div>
+        );
+      }
+      return null;
+    });
+  };
+
+  return (
+    <div className="container my-5">
+      <div className="section">
+        <h2>Rekomendasi Menu</h2>
+        <div className="food-display-list">
+          {recommendationResponse ? renderMenuItems(recommendationResponse) : "Loading..."}
+        </div>
       </div>
-      <hr />
+      <div className="section mt-5">
+        <h2>Best Seller</h2>
+        <div className="food-display-list">
+          {bestSellerResponse ? renderMenuItems(bestSellerResponse) : "Loading..."}
+        </div>
+      </div>
     </div>
   );
 };
