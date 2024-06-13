387e716d47887497ed314429e7767fb19c4a6aef
dianafnioktavia23 <113124849+dianafnioktavia23@users.noreply.github.com>
2024-06-13 16:51:13 +0700

    -

--- src/pages/HubungiKami/HubungiKami.jsx
+++ src/pages/HubungiKami/HubungiKami.jsx
@@ -1,8 +1,9 @@
 import "./HubungiKami.css";
 import React, { useState } from "react";
 import axios from "axios";
-import Header from "../../components/Header/Header";
+import { useNavigate } from "react-router-dom";
 import Banner from "../../components/Navbar/Banner";
+import { Toaster, toast } from 'react-hot-toast';
 
 const HubungiKami = () => {
   const [formData, setFormData] = useState({
@@ -12,6 +13,8 @@ const HubungiKami = () => {
   });
 
   const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -26,60 +29,75 @@ const HubungiKami = () => {
     axios
       .post("http://localhost:8000/api/send-email", formData)
       .then((response) => {
-        setMessage("Email sent successfully!");
-        setFormData({ nama: "", email: "", pesan: "" }); // Clear form inputs
+        toast.success('Email sent successfully!');
+        setFormData({ nama: "", email: "", pesan: "" });
+        setSubmitted(true);
       })
       .catch((error) => {
-        setMessage("Failed to send email.");
+        toast.error('Failed to send email.');
       });
   };
 
   return (
     <>
-    <Banner />
-    
-      <div>
-      </div>
+      <Banner />
       <div className="contact-us">
-        <h1>Hubungi Kami</h1>
-        <p>Silahkan tinggalkan pesan anda pada kolom yang tersedia.</p>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="nama">Nama:</label>
-            <input
-              type="text"
-              id="nama"
-              name="nama"
-              value={formData.nama}
-              onChange={handleChange}
-              required
-            />
+        {submitted ? (
+          <div className="thank-you">
+            <h2>Terima Kasih!</h2>
+            <p>Sudah Mengisi Ulasan Kami.</p>
+            <div>
+              <button onClick={() => navigate('/')}>Kembali ke halaman utama</button>
+            </div>
           </div>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="pesan">Pesan:</label>
-            <textarea
-              id="pesan"
-              name="pesan"
-              value={formData.pesan}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <button type="submit">Kirim</button>
-        </form>
-        {message && <p className="message">{message}</p>}
+        ) : (
+          <>
+            <div className="left-section">
+              <h1>Hubungi Kami</h1>
+              <p>Silahkan tinggalkan pesan anda pada kolom yang tersedia.</p>
+            </div>
+            <div className="right-section">
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="nama">Nama:</label>
+                  <input
+                    type="text"
+                    id="nama"
+                    name="nama"
+                    value={formData.nama}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="email">Email:</label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="pesan">Pesan:</label>
+                  <textarea
+                    id="pesan"
+                    name="pesan"
+                    value={formData.pesan}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <button type="submit">Kirim</button>
+              </form>
+              {message && <p className="message">{message}</p>}
+            </div>
+          </>
+        )}
       </div>
+      <Toaster />
     </>
   );
 };
--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,202 +1,242 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import "./Menu.css";
 import NominalFormat from "../../components/NumberFormat/NumberFormat";
 import Banner from "../../components/Navbar/Banner";
 import { StoreContext } from "../../context/StoreContext";
 
 const Menu = () => {
-	const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
-	const [menuItems, setMenuItems] = useState([]);
-	const [category, setCategory] = useState("Semua");
-	const [selectedMenu, setSelectedMenu] = useState(null);
-	const navigate = useNavigate();
+  const { cartItems, addToCart, updateKeterangan } = useContext(StoreContext);
+  const [menuItems, setMenuItems] = useState([]);
+  const [category, setCategory] = useState("Semua");
+  const [selectedMenu, setSelectedMenu] = useState(null);
+  const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate('/cart');
+    navigate("/cart");
   };
-	// const [cart, setCart] = useState([]);
+  // const [cart, setCart] = useState([]);
 
-	useEffect(() => {
-		const fetchMenuItems = async () => {
-			try {
-				let response;
-				if (category === "Semua") {
-					response = await axios.get("http://127.0.0.1:8000/api/menu/default");
-				} else if (category === "10") {
-					response = await axios.get(
-						"http://127.0.0.1:8000/api/menu/kategori/10"
-					);
-				} else if (category === "11") {
-					response = await axios.get(
-						"http://127.0.0.1:8000/api/menu/kategori/11"
-					);
-				} else if (category === "12") {
-					response = await axios.get(
-						"http://127.0.0.1:8000/api/menu/kategori/12"
-					);
-				} else {
-					console.error("Unknown category:", category);
-					return;
-				}
-				setMenuItems(response.data);
-			} catch (error) {
-				console.error("Error fetching menu items:", error);
-			}
-		};
+  useEffect(() => {
+    const fetchMenuItems = async () => {
+      try {
+        let response;
+        if (category === "Semua") {
+          response = await axios.get("http://127.0.0.1:8000/api/menu/default");
+        } else if (category === "10") {
+          response = await axios.get(
+            "http://127.0.0.1:8000/api/menu/kategori/10"
+          );
+        } else if (category === "11") {
+          response = await axios.get(
+            "http://127.0.0.1:8000/api/menu/kategori/11"
+          );
+        } else if (category === "12") {
+          response = await axios.get(
+            "http://127.0.0.1:8000/api/menu/kategori/12"
+          );
+        } else {
+          console.error("Unknown category:", category);
+          return;
+        }
+        setMenuItems(response.data);
+      } catch (error) {
+        console.error("Error fetching menu items:", error);
+      }
+    };
 
-		fetchMenuItems();
-	}, [category]);
+    fetchMenuItems();
+  }, [category]);
 
-	const handleCategoryChange = (newCategory) => {
-		setCategory(newCategory);
-	};
+  const handleCategoryChange = (newCategory) => {
+    setCategory(newCategory);
+  };
 
-	const handleMenuClick = async (menuId) => {
-		if (selectedMenu && selectedMenu.id === menuId) {
-			setSelectedMenu(null); // Hide details if the same menu is clicked again
-		} else {
-			try {
-				const response = await axios.get(`http://127.0.0.1:8000/api/menu/${menuId}`);
-				setSelectedMenu(response.data);
-			} catch (error) {
-				console.error("Error fetching menu details:", error);
-			}
-		}
-	};
-	
+  const handleMenuClick = async (menuId) => {
+    if (selectedMenu && selectedMenu.id === menuId) {
+      setSelectedMenu(null); // Hide details if the same menu is clicked again
+    } else {
+      try {
+        const response = await axios.get(
+          `http://127.0.0.1:8000/api/menu/${menuId}`
+        );
+        setSelectedMenu(response.data);
+      } catch (error) {
+        console.error("Error fetching menu details:", error);
+      }
+    }
+  };
 
-	// const addToCart = (item) => {
-	// 	console.log("cart", cart)
-	// 	console.log("item", item)
-	// 	setCart([...cart, item]);
-	// };
+  // const addToCart = (item) => {
+  // 	console.log("cart", cart)
+  // 	console.log("item", item)
+  // 	setCart([...cart, item]);
+  // };
 
-	const cumTotalCart = () => {
-		let total = 0
-		for (let i = 0; i < cartItems.length; i++) {
-			total += Number(cartItems[i].harga)
-		}
-		return total
-	};
+  const cumTotalCart = () => {
+    let total = 0;
+    for (let i = 0; i < cartItems.length; i++) {
+      total += Number(cartItems[i].harga);
+    }
+    return total;
+  };
 
-	
+  const handleKeterangan = (e, item) => {
+    // console.log("item", item);
+    let c = {
+      id: item.id,
+      keterangan: e,
+      harga: item.harga,
+      nama_menu: item.nama_menu,
+      deskripsi: item.deskripsi,
+      kategori: item.kategori,
+      gambar: item.gambar,
+      stok: 1,
+    };
+    updateKeterangan(c);
+  };
 
-	return (
-		<>
-		<Banner />
-		<div className="semua-menu">
-			<div className="filter">
-				<button
-					className="filter-button"
-					onClick={() => handleCategoryChange("Semua")}
-				>
-					Semua
-				</button>
-				<button
-					className="filter-button"
-					onClick={() => handleCategoryChange("10")}
-				>
-					Makanan
-				</button>
-				<button
-					className="filter-button"
-					onClick={() => handleCategoryChange("11")}
-				>
-					Minuman
-				</button>
-				<button
-					className="filter-button"
-					onClick={() => handleCategoryChange("12")}
-				>
-					Cemilan
-				</button>
-			</div>
-			<div className="menu-container">
+  return (
+    <>
+      <Banner />
+      <div className="semua-menu">
+        <div className="filter">
+          <button
+            className="filter-button"
+            onClick={() => handleCategoryChange("Semua")}
+          >
+            Semua
+          </button>
+          <button
+            className="filter-button"
+            onClick={() => handleCategoryChange("10")}
+          >
+            Makanan
+          </button>
+          <button
+            className="filter-button"
+            onClick={() => handleCategoryChange("11")}
+          >
+            Minuman
+          </button>
+          <button
+            className="filter-button"
+            onClick={() => handleCategoryChange("12")}
+          >
+            Cemilan
+          </button>
+        </div>
+        <div className="menu-container">
+          <div
+            className="menu-items"
+            style={{
+              gridTemplateColumns:
+                selectedMenu != null ||
+                (cartItems != null && cartItems.length > 0)
+                  ? "repeat(3, 1fr)"
+                  : "repeat(4, 1fr)",
+            }}
+          >
+            {menuItems.map((item, index) => {
+            //   console.log("item", item);
+              const imageUrl = `http://127.0.0.1:8081/images/${item.gambar}`;
+              return (
+                <div className="menu-item" key={index}>
+                  <img
+                    src={imageUrl}
+                    alt={item.nama_menu}
+                    className="menu-item-image"
+                    onClick={() => handleMenuClick(item.id)}
+                  />
+                  <h3 className="menu-item-name">{item.nama_menu}</h3>
+                  <p className="menu-item-description">
+                    {item.deskripsi} <a href="#detail">Detail</a>
+                  </p>
+                  <div className="menu-item-footer">
+                    <span className="menu-item-price">{item.harga}</span>
+                    <button
+                      className="add-to-cart-button"
+                      onClick={() => addToCart(item)}
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          {selectedMenu != null ? (
+            <div className="menu-cart-frame">
+              <div className={`menu-details ${selectedMenu ? "active" : ""}`}>
+                {selectedMenu && (
+                  <div>
+                    <h2>{selectedMenu.nama_menu}</h2>
+                    <img
+                      src={selectedMenu.gambar}
+                      alt={selectedMenu.nama_menu}
+                    />
+                    <p>
+                      <strong>Nama:</strong> {selectedMenu.nama_menu}
+                    </p>
+                    <p>
+                      <strong>Deskripsi:</strong> {selectedMenu.deskripsi}
+                    </p>
+                    <p>
+                      <strong>Harga:</strong> {selectedMenu.harga}
+                    </p>
 
-
-				<div className="menu-items" style={{ gridTemplateColumns: (selectedMenu != null || (cartItems != null && cartItems.length > 0)) ? 'repeat(3, 1fr)' : 'repeat(4, 1fr)' }}>
-					{menuItems.map((item, index) => {
-						console.log("item", item)
-						const imageUrl = `http://127.0.0.1:8000/assets/images/${item.gambar}`;
-						return (
-							<div className="menu-item" key={index}>
-								<img
-									src={imageUrl}
-									alt={item.nama_menu}
-									className="menu-item-image"
-									onClick={() => handleMenuClick(item.id)}
-								/>
-								<h3 className="menu-item-name">{item.nama_menu}</h3>
-								<p className="menu-item-description">
-									{item.deskripsi} <a href="#detail">Detail</a>
-								</p>
-								<div className="menu-item-footer">
-									<span className="menu-item-price">{item.harga}</span>
-									<button
-										className="add-to-cart-button"
-										onClick={() => addToCart(item)}
-									>
-										+
-									</button>
-								</div>
-							</div>
-						);
-					})}
-				</div>
-				{
-					selectedMenu != null ?
-						<div className="menu-cart-frame">
-							<div className={`menu-details ${selectedMenu ? "active" : ""}`}>
-								{selectedMenu && (
-									<div>
-										<h2>{selectedMenu.nama_menu}</h2>
-										<img src={selectedMenu.gambar} alt={selectedMenu.nama_menu} />
-										<p><strong>Nama:</strong> {selectedMenu.nama_menu}</p>
-										<p><strong>Deskripsi:</strong> {selectedMenu.deskripsi}</p>
-										<p><strong>Harga:</strong> {selectedMenu.harga}</p>
-										<button className="back-button" onClick={() => setSelectedMenu(null)}>
-											Kembali
-										</button>
-									</div>
-								)}
-							</div>
-						</div> : null
-				}
-				{
-					cartItems != null && cartItems.length > 0 ?
-						<div className="menu-cart-frame menu-card-order-box">
-							Daftar Pemesanan
-							<div className="mb-5">
-								{
-									cartItems.map((item, index) => {
-
-										return (
-											<div className="menu-card-order-box-item" key={index}>
-												{item.nama_menu}
-												<NominalFormat className={""} value={item.harga} />
-											</div>
-										);
-									})
-								}
-							</div>
-							<label htmlFor="">Keterangan</label>
-							<textarea className="textarea-form" id="" cols="30" rows="10"></textarea>
-
-							test total:
-							<NominalFormat displayType={'text'} className={""} dis value={cumTotalCart()} />
-							<br/>
-							<button className="btn btn-primary" onClick={handleClick}>
-							Pesan
-							</button>
-							{/* <button onClick={() => setCart([])}>Test Pesan</button> */}
-						</div> : null
-				}
-			</div>
-		</div>
-		</>
-	);
+                    <button
+                      className="back-button"
+                      onClick={() => setSelectedMenu(null)}
+                    >
+                      Kembali
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+          ) : null}
+          {cartItems != null && cartItems.length > 0 ? (
+            <div className="menu-cart-frame menu-card-order-box">
+              Daftar Pemesanan
+              <div className="mb-5">
+                {cartItems.map((item, index) => {
+                //   console.log("item", item);
+                  return (
+                    <div className="menu-card-order-box-item" key={index}>
+                      {item.nama_menu}
+                      <NominalFormat className={""} value={item.harga} />
+                      <p>
+                        <strong>Keterangan:</strong>
+                        <textarea
+                          onChange={(e) =>
+                            handleKeterangan(e.target.value, item)
+                          }
+                        ></textarea>
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+              test total:
+              <NominalFormat
+                displayType={"text"}
+                className={""}
+                dis
+                value={cumTotalCart()}
+              />
+              <br />
+              <button className="btn btn-primary" onClick={handleClick}>
+                Pesan
+              </button>
+              {/* <button onClick={() => setCart([])}>Test Pesan</button> */}
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Menu;
